ff0e18cdd891f8780caf91ae1d75d2ed1e89d34e
iMunnanAsim <munnanasim@ucp.edu.pk>
2018-11-23 11:31:08 +0500

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	project-demo/admin/insert_product.html

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -175,23 +175,107 @@ show();
 */
 
 
-/*Elememts are in a table and delete button is working so you can delete any todolist with it and todolist remanins saved  /*
+/*Elememts are in a table and delete button is working so you can delete any todolist with it and todolist remanins saved  */
 
 
 //============================================
 //-----------  STEP - 5 ----------------------
 //============================================
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
 
-/*
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
 
-CLASS ACTIVITY (HOME WORK) :
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' +'<p  id="\'+i+\'" onclick="strikethroug(this)">'+ todos[i].task +'</p>' +'</li>' +
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>'+
+            '<button class="btn btn-reset" id1="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> reset </span> </button>';
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    var buttons2 = document.getElementsByClassName('btn-reset');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+        buttons2[i].addEventListener('click',resetval);
+    }
+}
+function resetval(){
+    var id = this.getAttribute('id1');
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.splice(id,1);
+    todos.push({task: task, isDone: false});
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
 
-1- Enable State of todos by clicking on the text completed, started etc
-HINT: use text-decoration:line-through; property of CSS
 
-2- Enable Editing todos in text field to update text
+function strikethroug(ele)
+{
+         var x = ele.style.color ="red";
+}
 
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
 
-
-*/
 
